2394509c604c3e68d375b8247ff40ee4ed9094e3
Heli <heliribeiro2@gmail.com>
2023-10-21 20:18:43 -0300

    feat: setting up the project with nativewind

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className='flex-1 bg-red-500 justify-center items-center'>
+      <Text className='text-white'>Open up App.tsx to start working on your app!</Text>
+      <StatusBar/>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
